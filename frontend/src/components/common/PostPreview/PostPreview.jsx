39defa0126df3cc89b69bb70dcc3d9174c84bfac
Bit-Watcher <shaunmarx05@gmail.com>
2026-09-02 09:30:33 +0200

    Start with ProfilePreview

---
+++ frontend/src/components/common/PostPreview/PostPreview.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import {Form, Button, Container, Row, Col} from 'react-bootstrap';
+
+
+// just image, like, date and who posted it. To see comments need to click on it. Might add a comment button like in insta
+//don't know if we need states. Just pulling data from somewhere and showing it so probs not
+//array == user, likes, date, img 
+//used for home page
+function ProfilePreview(){
+
+    return (
+        
+    )
+}
+
+
+export default ProfilePreview;
